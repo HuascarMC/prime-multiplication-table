165d47f4f5acfa21c81568e61070b15051542409
Huascar <huascarmc@hotmail.com>
2018-04-02 17:38:57 +0100

    added test is prime, is NOT prime

---
+++ src/tools/primable/Primable.js
@@ -0,0 +1,23 @@
+var Primable = function () {
+
+}
+
+Primable.prototype.isPrime = function(number) {
+ if(number % 2 === 0 && number !== 2) {
+  return false
+ }
+ return true
+}
+
+Primable.prototype.getPrimes = function(number) {
+ var result = [];
+ for(var i = 2; i < number; i++) {
+   if(this.isPrime(i)) {
+    result.push(i);
+   }
+  }
+ return result;
+}
+
+
+module.exports = Primable;
--- src/tools/primable/Primable.test.js
+++ src/tools/primable/Primable.test.js
@@ -9,10 +9,18 @@ describe('Primable', function () {
   primable = new Primable();
  });
 
+ it('should check if a number is prime', () {
+  assert.strictEqual(primable.isPrime(7), true)
+ });
+
+ it('should check if a number is not prime', () {
+  assert.strictEqual(primable.isPrime(4), false)
+ })
+
  it('should get all prime numbers from zero to a given number', function () {
   const number = 10;
   result = [2, 3, 5, 7, 9];
-  assert.strictEqual(primable.getPrimes(number), );
+  assert.strictEqual(primable.getPrimes(number), result);
   });
 
  });
