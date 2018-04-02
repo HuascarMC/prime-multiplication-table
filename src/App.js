1b52d8638d9066d205f52fda7a8ca65ea4ea2e18
Huascar <huascarmc@hotmail.com>
2018-04-02 21:18:20 +0100

    inputcontainer compiled success in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import InputContainer from './containers/InputContainer/InputContainer.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div><p>setup</p></div>
+      <InputContainer />
     );
   }
 }
