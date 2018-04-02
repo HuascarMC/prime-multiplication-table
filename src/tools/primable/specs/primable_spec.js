f750be93de4228c97ba8e9ad20c0fbba02c0fc81
Huascar <huascarmc@hotmail.com>
2018-04-02 17:57:59 +0100

    added test increaseArray method

--- src/tools/primable/specs/primable_spec.js
+++ src/tools/primable/specs/primable_spec.js
@@ -15,12 +15,18 @@ describe('Primable', function () {
 
  it('should check if a number is not prime', function () {
   assert.strictEqual(primable.isPrime(4), false)
- })
+ });
 
  it('should get all prime numbers from zero to a given number', function () {
   const number = 10;
   result = primable.getPrimes(number)
   assert.strictEqual(result.length, 5);
-  });
-
  });
+
+ it('should increase array by a given value', function () {
+  array = [2, 3, 5, 7, 9]
+  result = primable.increaseArray(array, 2)
+  assert.strictEqual(result, array)
+ });
+
+});
