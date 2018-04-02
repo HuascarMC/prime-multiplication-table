0dab80186353285d95176b8815db5de7a6d74ea1
Huascar <huascarmc@hotmail.com>
2018-04-02 22:55:20 +0100

    appendrows method fixed bug

--- src/components/TableRowComponent/TableRowComponent.js
+++ src/components/TableRowComponent/TableRowComponent.js
@@ -14,9 +14,11 @@ class TableRowComponent extends React.Component {
 appendRows(array) {
   for(var i = -1; i < array.length; i++) {
    if (i == -1) {
-    array.push(0)
-    var firstRow = this.generateRow(array);
+    var indexArray = this.state.primable.increaseArray(array, 1);
+    indexArray.unshift("Nice")
+    var firstRow = this.generateRow(indexArray);
     this.state.appendedRows.push(firstRow);
+
    } else {
     var increasedArray = this.state.primable.increaseArray(array, array[i]);
     increasedArray.unshift(array[i])
