c076ff94dd30f25a6e6ffe7eb1fc93f9ffb0346a
Huascar <huascarmc@hotmail.com>
2018-04-02 13:39:49 +0100

    added primes array state test

--- src/containers/TableContainer/TableContainer.test.js
+++ src/containers/TableContainer/TableContainer.test.js
@@ -6,3 +6,9 @@ import renderer from 'react-test-renderer';
 it('should render without crashing', () => {
  shallow(<TableContainer />);
 })
+
+it('has a primes array state', () => {
+ const wrapper = shallow(<TableContainer />);
+ const state = wrapper.state();
+ expect(state.primes().length).toEqual(0)
+})
