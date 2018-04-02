5effad6558f563143e57e3e034e1d91246fabdfa
Huascar <huascarmc@hotmail.com>
2018-04-02 21:30:04 +0100

    added primable tests table container, snapshot test fail after change

--- src/containers/TableContainer/TableContainer.test.js
+++ src/containers/TableContainer/TableContainer.test.js
@@ -26,3 +26,37 @@ it('should match the snapshot', () => {
  let tree = component.toJSON();
  expect(tree).toMatchSnapshot();
 })
+
+
+it('should be able to use primable class', () => {
+ const wrapper = shallow(<TableContainer />);
+ const primable = wrapper.state().primable
+
+ expect(primable).toBeTruthy();
+})
+
+it('should be able to use primable getPrimes method', () => {
+ const wrapper = shallow(<TableContainer />);
+ const primable = wrapper.state().primable
+
+ const result = primable.getPrimes(10)
+ expect(result.length).toEqual(5);
+})
+
+it('should be able to use primable increaseArray method', () => {
+ const wrapper = shallow(<TableContainer />);
+ const primable = wrapper.state().primable
+
+ const primesArray = primable.getPrimes(10);
+ const increasedArray = primable.increaseArray(primesArray, 3);
+ const result = [6, 9, 15, 21, 27];
+ expect(increasedArray).toEqual(result);
+})
+
+it('should be able to use primable is Prime method', () => {
+ const wrapper = shallow(<TableContainer />);
+ const primable = wrapper.state().primable
+
+ const result = primable.isPrime(7);
+ expect(result).toEqual(true);
+})
