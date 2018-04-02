aad12c06fd90db3af14b151f147ef61d0faa8d46
Huascar <huascarmc@hotmail.com>
2018-04-02 16:23:58 +0100

    generateRow method test added

--- src/components/TableRowComponent/TableRowComponent.test.js
+++ src/components/TableRowComponent/TableRowComponent.test.js
@@ -12,3 +12,11 @@ it('has an initial current row state empty', () => {
  const state = wrapper.state();
  expect(state.currentRow.length).toEqual(0);
 })
+
+it('generates a row given an array', () => {
+ const wrapper = shallow(<TableRowComponent />);
+ const row = wrapper.generateRow([1,2,3]);
+
+ const result = <tr><td>1</td><td>2</td><td>3</td></tr>
+ expect(row).toEqual(result)
+})
