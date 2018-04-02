6fc96e583e747b273b01d67b85ad4b31809edb81
Huascar <huascarmc@hotmail.com>
2018-04-02 13:06:00 +0100

    created tablecontainer class renders without crashing test passed

---
+++ src/containers/TableContainer/TableContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TableContainer extends React.Component {
+ constructor(props) {
+  super(props);
+}
+
+ render() {
+  <div></div>
+ }
+}
+
+export default TableContainer;
