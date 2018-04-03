e94d9aac27510e9b354c1e5c4123117a1eb05863
Huascar <huascarmc@hotmail.com>
2018-04-03 10:39:04 +0100

    added test for values greater than 500, needs improvements; e.preventdefault causes issues

--- src/containers/InputContainer/InputContainer.test.js
+++ src/containers/InputContainer/InputContainer.test.js
@@ -40,7 +40,7 @@ it('has an initial number state of zero', () => {
 it('DEPRECATED: should update state on input change', () => {
  const wrapper = shallow(<InputContainer />);
  const input = wrapper.find('.get-number-input');
-
+ // needs improvement, can't change value of input from text when having e.prevent default attribute.
  wrapper.find('.get-number-input').simulate('submit',  { preventDefault: () => {}, target: {value: 1} })
 
  expect(wrapper.state().number).toEqual(0)
@@ -51,7 +51,7 @@ it('DEPRECATED: should update display user input', () => {
  const input = wrapper.find('.get-number-input');
 
  input.simulate('submit', { preventDefault: () => {}, target: {value: '20'} })
-
+ // needs improvement, can't change value of input from text when having e.prevent default attribute.
  expect(wrapper.find('.display-input-text').text()).toEqual("0")
 })
 
@@ -61,4 +61,20 @@ it('should match the snapshot', () => {
  );
  let tree = component.toJSON();
  expect(tree).toMatchSnapshot();
+})
+
+it('DEPRECATED: should not take input higher than five-hundred', () => {
+ const wrapper = shallow(<InputContainer />);
+ const input = wrapper.find('.get-number-input');
+ // needs improvement, can't change value of input from text when having e.prevent default attribute.
+ wrapper.find('.get-number-input').simulate('change', {target: {value: 501} })
+ expect(wrapper.state().number).toEqual(0);
+})
+
+it('DEPRECATED: shoul tell user to input a value less than five-hundred', () => {
+ const wrapper = shallow(<InputContainer />);
+ const input = wrapper.find('.get-number-input');
+ // needs improvement, can't change value of input from text when having e.prevent default attribute.
+ wrapper.find('.get-number-input').simulate('change', {target: {value: 501} })
+ expect(wrapper.state().number).toEqual(0);
 })
